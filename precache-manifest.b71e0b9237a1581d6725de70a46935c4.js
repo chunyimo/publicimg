a1b0215748f705501ed921f8796ece6ebbc16a8c
chunyi.mo <chunyi.mo@yitu-inc.com>
2020-09-16 17:01:47 +0800

    Updates

---
+++ precache-manifest.b71e0b9237a1581d6725de70a46935c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc737ec2c2eed64cacb057b6f7afc145",
+    "url": "./index.html"
+  },
+  {
+    "revision": "335439079b9853d11acf",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cf18544a652d63bb161f",
+    "url": "./static/js/2.f6545ac0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.f6545ac0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "335439079b9853d11acf",
+    "url": "./static/js/main.a7b5ab1b.chunk.js"
+  },
+  {
+    "revision": "9f3bb4411404c88614b5",
+    "url": "./static/js/runtime-main.0f081cb3.js"
+  }
+]);
